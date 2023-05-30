5cea1396555b5853ea395a12914236294b4377ca
AryanShelke <46510623+AryanShelke@users.noreply.github.com>
2023-05-29 20:31:41 -0700

    Working KNN Frontend

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 
 app = Flask(__name__)
 knn_model = pickle.load(open('knn_model.pkl','rb'))
+svd_model = pickle.load(open('svd_model.pkl','rb'))
 
 # Create ID to name dictionary
 def read_item_names():
@@ -22,6 +23,7 @@ def read_item_names():
     return rid_to_name, name_to_rid
 
 rid_to_name, name_to_rid = read_item_names()
+recommendation_count = 10
 
 @app.route('/')
 def home():
@@ -29,20 +31,37 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    print("Prediction Request")
+    print("KNN Prediction Request")
     """Grabs the input values and uses them to make prediction"""
-    anime_name = request.form["anime_name"]
+
+    anime_name = request.form["anime_name_knn"]
+    print(anime_name)
+
     try:    
         raw_id = name_to_rid[anime_name]
     except:
         print("Invalid Name")
         return render_template('index.html', prediction_text=f'Invalid Anime Name')
     
-    predictions = knn_model.get_neighbors(int(raw_id), k=10)  # this returns a list e.g. [127.20488798], so pick first element [0]
-    return render_template('index.html', prediction_text=f'{predictions}')
+    prediction_ids = knn_model.get_neighbors(int(raw_id), k=recommendation_count)  # this returns a list e.g. [127.20488798], so pick first element [0]
+    prediction_ids = list((knn_model.trainset.to_raw_iid(inner_id) for inner_id in prediction_ids))
+    prediction_names = [rid_to_name[str(rid)] for rid in prediction_ids]
     
-    
-    
+    predictions = ""
+    for i in range(recommendation_count):
+        predictions += prediction_names[i] + " (" + str(prediction_ids[i]) + ")\n"
+    return render_template('index.html', prediction_knn=f'{predictions}')
 
+@app.route('/predict2',methods=['POST'])
+def predict2():
+    print("SVD Prediction Request")
+    """Grabs the input values and uses them to make prediction"""
+    anime_name = request.form["anime_name_svd"]
+
+    item_count = 
+    item_factors = svd_model.qi
+
+    return render_template('index.html', prediction_svd=f'{predictions}')
+    
 if __name__ == '__main__':
     app.run()
